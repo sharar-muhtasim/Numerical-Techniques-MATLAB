1bc235638c11753c230fcf38c3ec9385b2483fa4
Sharar Muhtasim <60645992+sharar-muhtasim@users.noreply.github.com>
2020-03-04 16:10:05 +0600

    Add files via upload

---
+++ Lab6/sir_qsn_3.m
@@ -0,0 +1,42 @@
+clc;close all;clear all;
+f=@(x) (1-x)/sqrt(x);
+diff = @(x) -((x+1)/(2*x^1.5)); %ei part experiment korbo pore
+
+r=input('Input a value of root r: ');
+tol=input('Percentage of relative error tolerance: ');
+rel_error = 1;
+
+xr=r;
+i=1;
+while(rel_error > tol)
+   xr_old = xr;
+   xr = xr-(f(xr)/diff(xr));
+   xrnew(i) = xr;
+   rel_error(i) = abs((xr-xr_old)/xr)*100;
+   i=i+1;
+end
+fprintf('The  root of the function is %f', xr);
+
+n=1:1:i-1;
+figure;
+subplot(311)
+plot(n,rel_error,'-ro')
+grid on
+xlabel('number of iteration n')
+ylabel('percentage of relative error')
+
+m=0:1:i-1;
+xru =[r xrnew];
+subplot(312)
+plot(m,xru,'-bs')
+xlabel('number of iteration n')
+ylabel('updated xr at each iteration')
+grid on
+
+f_xru=subs(f,xru);
+subplot(313)
+plot(m,f_xru,'-gp')
+xlabel('number of iteration n')
+ylabel('function value for each updated xr')
+grid on
+
